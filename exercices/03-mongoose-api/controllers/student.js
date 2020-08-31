4f2f9ab5c8c0b8157c48fac5a96c9b92f3324f94
gilles-bertrand <gillestriptyk.eu>
2020-08-31 12:04:13 +0200

    last errors

--- exercices/03-mongoose-api/controllers/student.js
+++ exercices/03-mongoose-api/controllers/student.js
@@ -1,4 +1,4 @@
-const model = require('../models/student');
+
 const student = require('../models/student');
 
 exports.update = async (req, res) => {
@@ -15,7 +15,7 @@ exports.update = async (req, res) => {
                 body = '';
                 req.on('data', data => { body += data; })
                 req.on('end', async () => {
-                    let data = await model.findByIdAndUpdate(id, JSON.parse(body),{new:true})
+                    let data = await student.findByIdAndUpdate(id, JSON.parse(body),{new:true})
                     res.setHeader('Content-Type', 'application/json');
                     if (data._id) {
                         response.status = 200;
@@ -56,7 +56,7 @@ exports.delete = async (req, res) => {
             res.message = 'Bad parameter';
             res.end(JSON.stringify(res));
         } else {
-            let data = await model.findByIdAndDelete(id)
+            let data = await student.findByIdAndDelete(id)
             res.setHeader('Content-Type', 'application/json');
             if (data._id) {
                 res.status = 200;
@@ -83,7 +83,7 @@ exports.delete = async (req, res) => {
 
 exports.list = async (req, res) => {
     try {
-        let data = await model.find({});
+        let data = await student.find({});
         res.statusCode = 200;
         //  res.setHeader('Content-Type: application/json');
         res.end(JSON.stringify(data))
@@ -103,7 +103,7 @@ exports.read = async (req, res) => {
     try {
         let values = req.url.split('/');
         let id = values[values.length - 1];
-        let data = await model.findById(id);
+        let data = await student.findById(id);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(data))
